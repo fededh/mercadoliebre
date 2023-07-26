05491635846249cf6a692545feac1c8157c4aec1
Fede Garcia <fgarcia@digitalhouse.com>
2023-07-26 19:43:52 -0300

    nuestro primer commit de mercadoliebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+
+
+app.listen(PORT, ()=>{
+    console.log('Servidor funcionando en el puerto: ' + PORT);
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
